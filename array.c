a17227f1e9de0430d513743dbc28b04a7b754a98
shailendra rajak <rajakshailendra17@gmail.com>
2026-09-02 19:25:43 +0530

    input

--- array.c
+++ array.c
@@ -2,11 +2,16 @@
 void main()
 {
     int marks[] = {40, 50, 80, 90, 55, 65};
-    int i;
+    int i, student[5];
     for (i = 0; i <= 5; i++)
     {
         printf("%d\t", marks[i]);
     }
     printf("\nsize of marks in bytes:");
     printf("%d", sizeof(marks));
+    printf("\nenter elements:");
+    for (i = 0; i < 5; i++)
+    {
+        scanf("%d", &student[i]);
+    }
 }
