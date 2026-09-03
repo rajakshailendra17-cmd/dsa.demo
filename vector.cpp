94165c1cc68a3bc4d73c1017b3656665ece211f7
shailendra rajak <rajakshailendra17@gmail.com>
2026-09-03 18:49:55 +0530

    introduce popback

--- vector.cpp
+++ vector.cpp
@@ -4,11 +4,16 @@ int main()
 {
     std::vector<int> vec;
     vec.push_back(10);
+    vec.push_back(20);
+    vec.push_back(40);
+    vec.push_back(50);
+    vec.push_back(60);
+    vec.pop_back();
     printf("size of vector:%d", vec.size());
-    // for (int i = 0; i < vec.size(); i++)
-    // {
-    //     std::printf("%d ", vec[i]);
-    // }
-    printf("\n%d", vec[0]);
+    for (int i = 0; i < vec.size(); i++)
+    {
+        printf("\n%d ", vec[i]);
+    }
+
     return 0;
 }
