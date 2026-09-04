ff38e4a4116d1e4610f6ea183d128d548525d80b
shailendra rajak <rajakshailendra17@gmail.com>
2026-09-04 17:47:54 +0530

    singlenumber concept

--- vector.cpp
+++ vector.cpp
@@ -1,26 +1,43 @@
 #include <stdio.h>
 #include <vector>
+class Solution
+{
+public:
+    int singleNumber(std::vector<int> &vec)
+    {
+        int ans = 0;
+        for (int i = 0; i < vec.size(); i++)
+        {
+            ans = ans ^ vec[i];
+        }
+        return ans;
+    }
+};
 int main()
 {
-    std::vector<int> vec;
+    std::vector<int> vec; // 0
     vec.push_back(10);
     vec.push_back(20);
     vec.push_back(40);
-    vec.push_back(50);
-    vec.push_back(60);
-    vec.pop_back();
+    vec.push_back(20);
+    vec.push_back(10);
+    // vec.pop_back();
     printf("size of vector:%d", vec.size());
-    for (int i = 0; i < vec.size(); i++)
-    {
-        printf("\n%d ", vec[i]);
-    }
+    // for (int i = 0; i < vec.size(); i++)
+    // {
+    //     printf("\n%d ", vec[i]);
+    // }
     // printf("\nfront value is:");
     // printf("%d", vec.front());
     // printf("\nend value is :");
     // printf("%d", vec.back());
     // printf("\ndisplay value of any index of vector:");
     // printf("\n%d", vec.at(2));
-    printf("\ncapacity of vector is:");
-    printf("%d", vec.capacity());
+    // printf("\ncapacity of vector is:");
+    // printf("%d", vec.capacity());
+    Solution sol;
+    int result = sol.singleNumber(vec);
+    printf("\nSingle number is: %d", result);
+
     return 0;
 }
