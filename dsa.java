c998c917cd63be9b2fa24917545810e1a7e2f20c
shailendra rajak <rajakshailendra17@gmail.com>
2026-09-07 20:29:59 +0530

    data types

--- dsa.java
+++ dsa.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class dsa {
     public static void main(String[] args) {
         // System.out.println("Hello, World!");
-        int a = 10;
-        int b = 20;
-        int sum = a + b;
-        System.out.println("Sum of a and b is: " + sum);
+        // int a = 10;
+        // int b = 20;
+        // int sum = a + b;
+        // System.out.println("Sum of a and b is: " + sum);
+        String name = "programmer";
+        System.out.println("Hello, " + name);
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        long b = sc.nextLong();
+        char c = sc.next().charAt(0);
+        float d = sc.nextFloat();
+        double e = sc.nextDouble();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+        sc.close();
 
     }
 }
