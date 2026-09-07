349d517b562c3d3ebf0611ddd6ca9f868961d4b7
shailendra rajak <rajakshailendra17@gmail.com>
2026-09-07 19:39:43 +0530

    summation

--- dsa.java
+++ dsa.java
@@ -1,6 +1,10 @@
 public class dsa {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("Sum of a and b is: " + sum);
 
     }
 }
