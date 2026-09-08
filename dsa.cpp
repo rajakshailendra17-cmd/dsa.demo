500e5c71834f179059bfb74c01a591ebaa68e373
shailendra rajak <rajakshailendra17@gmail.com>
2026-09-08 13:04:26 +0530

    simple interest

--- dsa.cpp
+++ dsa.cpp
@@ -4,13 +4,37 @@ using namespace std;
 
 int main()
 {
-    double X1, Y1, X2, Y2;
-    double distance;
-    cout << "Enter the coordinates of the first point (X1, Y1): ";
-    cin >> X1 >> Y1;
-    cout << "Enter the coordinates of the second point (X2, Y2): ";
-    cin >> X2 >> Y2;
-    distance = sqrt(pow(X2 - X1, 2) + pow(Y2 - Y1, 2));
-    cout << "The distance between the two points is: " << distance << endl;
-    return 0;
+    // double X1, Y1, X2, Y2;
+    // double distance;
+    // cout << "Enter the coordinates of the first point (X1, Y1): ";
+    // cin >> X1 >> Y1;
+    // cout << "Enter the coordinates of the second point (X2, Y2): ";
+    // cin >> X2 >> Y2;
+    // distance = sqrt(pow(X2 - X1, 2) + pow(Y2 - Y1, 2));
+    // cout << "The distance between the two points is: " << distance << endl;
+    // return 0;
+    // int n;
+    // cout << "Enter any number: ";
+    // cin >> n;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         if(j%i==0)
+    //         {
+    //             count++;
+    //         }
+    //  }
+    // cout << endl;
+    //}
+    int p;
+    float r, t, si;
+    cout << "Enter principal amount: ";
+    cin >> p;
+    cout << "Enter rate of interest: ";
+    cin >> r;
+    cout << "Enter time in years: ";
+    cin >> t;
+    si = (p * r * t) / 100;
+    cout << "Simple Interest = " << si << endl;
 }
--- dsa.java
+++ dsa.java
@@ -2,25 +2,25 @@ import java.util.Scanner;
 
 public class dsa {
     public static void main(String[] args) {
-        // System.out.println("Hello, World!");
-        // int a = 10;
-        // int b = 20;
-        // int sum = a + b;
-        // System.out.println("Sum of a and b is: " + sum);
         String name = "programmer";
         System.out.println("Hello, " + name);
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        long b = sc.nextLong();
-        char c = sc.next().charAt(0);
-        float d = sc.nextFloat();
-        double e = sc.nextDouble();
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
 
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(d);
-        System.out.println(e);
+        System.out.println(X + " + " + Y + " = " + (X + Y));
+        System.out.println(X + " * " + Y + " = " + (X * Y));
+        System.out.println(X + " - " + Y + " = " + (X - Y));
+        // long b = sc.nextLong();
+        // char c = sc.next().charAt(0);
+        // float d = sc.nextFloat();
+        // double e = sc.nextDouble();
+
+        // System.out.println(a);
+        // System.out.println(b);
+        // System.out.println(c);
+        // System.out.println(d);
+        // System.out.println(e);
 
         sc.close();
 
