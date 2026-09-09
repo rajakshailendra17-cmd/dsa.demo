2d11e5119c11f154b8378f5b6aaeb6e352e4b53a
shailendra rajak <rajakshailendra17@gmail.com>
2026-09-09 12:46:30 +0530

    difference

--- dsa.java
+++ dsa.java
@@ -2,25 +2,14 @@ import java.util.Scanner;
 
 public class dsa {
     public static void main(String[] args) {
-        String name = "programmer";
-        System.out.println("Hello, " + name);
         Scanner sc = new Scanner(System.in);
-        long X = sc.nextLong();
-        long Y = sc.nextLong();
-
-        System.out.println(X + " + " + Y + " = " + (X + Y));
-        System.out.println(X + " * " + Y + " = " + (X * Y));
-        System.out.println(X + " - " + Y + " = " + (X - Y));
-        // long b = sc.nextLong();
-        // char c = sc.next().charAt(0);
-        // float d = sc.nextFloat();
-        // double e = sc.nextDouble();
-
-        // System.out.println(a);
-        // System.out.println(b);
-        // System.out.println(c);
-        // System.out.println(d);
-        // System.out.println(e);
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long C = sc.nextLong();
+        long D = sc.nextLong();
+        long X;
+        X = (A * B) - (C * D);
+        System.out.println("Difference = " + X);
 
         sc.close();
 
