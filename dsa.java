d5bb06f983f4ccddbf736294e30aabe7fc298e23
shailendra rajak <rajakshailendra17@gmail.com>
2026-09-09 13:10:09 +0530

    sum of last digit

--- dsa.java
+++ dsa.java
@@ -3,13 +3,11 @@ import java.util.Scanner;
 public class dsa {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        long A = sc.nextLong();
-        long B = sc.nextLong();
-        long C = sc.nextLong();
-        long D = sc.nextLong();
-        long X;
-        X = (A * B) - (C * D);
-        System.out.println("Difference = " + X);
+        long N = sc.nextLong();
+        long M = sc.nextLong();
+        long sum;
+        sum = (N % 10) + (M % 10);
+        System.out.println(sum);
 
         sc.close();
 
