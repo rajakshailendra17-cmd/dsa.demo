770772270d920ccc64670035f19b53d65d39bfa4
shailendra rajak <rajakshailendra17@gmail.com>
2026-09-11 14:11:56 +0530

    age in days

--- dsa.java
+++ dsa.java
@@ -3,23 +3,33 @@ import java.util.Scanner;
 public class dsa {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double X = sc.nextDouble();
-        double Y = sc.nextDouble();
-        if (X > 0 && Y > 0) {
-            System.out.println("Q1");
-        } else if (X < 0 && Y > 0) {
-            System.out.println("Q2");
-        } else if (X < 0 && Y < 0) {
-            System.out.println("Q3");
-        } else if (X > 0 && Y < 0) {
-            System.out.println("Q4");
-        } else if (X == 0 && Y == 0) {
-            System.out.println("Origem");
-        } else if (X == 0) {
-            System.out.println("Eixo Y");
-        } else if (Y == 0) {
-            System.out.println("Eixo X");
-        }
+        long N = sc.nextLong();
+        long years = N / 365;
+        N = N % 365;
+        long months = N / 30;
+        N = N % 30;
+        long days = N;
+        System.out.println(years + " " + "years");
+        System.out.println(months + " " + "months");
+        System.out.println(days + " " + "days");
+
+        // double X = sc.nextDouble();
+        // double Y = sc.nextDouble();
+        // if (X > 0 && Y > 0) {
+        // System.out.println("Q1");
+        // } else if (X < 0 && Y > 0) {
+        // System.out.println("Q2");
+        // } else if (X < 0 && Y < 0) {
+        // System.out.println("Q3");
+        // } else if (X > 0 && Y < 0) {
+        // System.out.println("Q4");
+        // } else if (X == 0 && Y == 0) {
+        // System.out.println("Origem");
+        // } else if (X == 0) {
+        // System.out.println("Eixo Y");
+        // } else if (Y == 0) {
+        // System.out.println("Eixo X");
+        // }
 
         // char firstDigit = sc.next().charAt(0);
         // int digit = firstDigit - '0';
