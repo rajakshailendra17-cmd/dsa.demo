1e48577a3bf96369cd77da103a884bf558ec63eb
shailendra rajak <rajakshailendra17@gmail.com>
2026-09-17 00:08:54 +0530

    number 1 to n

---
+++ loops.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class loops {
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+     int N = sc.nextInt();
+     int i = 1;
+     while(i<=N){
+        System.out.println(i);
+        i++;
+        
+     }
+     sc.close();
+
+
+}
+}
