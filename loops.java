6dcd2f7e8a84ed2e0600c2e75e242100a81da2fc
shailendra rajak <rajakshailendra17@gmail.com>
2026-09-17 00:19:14 +0530

    print even numbers

--- loops.java
+++ loops.java
@@ -3,14 +3,32 @@ import java.util.Scanner;
 public class loops {
     public static void main(String[] args) {
      Scanner sc = new Scanner(System.in);
+     //print all even number from 1 to N and print -1 if there is no even number.
      int N = sc.nextInt();
-     int i = 1;
+     int i = 2;
      while(i<=N){
         System.out.println(i);
-        i++;
+        i = i+2;
         
      }
-     sc.close();
+    
+     if(i==2){
+        System.out.println(-1);
+        
+     }
+
+
+sc.close();
+
+    //  int N = sc.nextInt();
+    //  int i = 1;
+    //  while(i<=N){
+    //     System.out.println(i);
+    //     i++;
+        
+    //  }
+     
+    
 
 
 }
