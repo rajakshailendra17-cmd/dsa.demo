b0645b902259e8489781e8917a5978c90d7d4197
shailendra rajak <rajakshailendra17@gmail.com>
2026-09-17 02:28:35 +0530

    check pallindrome

--- loops.java
+++ loops.java
@@ -2,145 +2,162 @@ import java.util.Scanner;
 
 public class loops {
     public static void main(String[] args) {
-     Scanner sc = new Scanner(System.in);
-     // Given a number X. Determine if the number is prime or not.
-    
-        int X = sc.nextInt();
-        int prime = 1;
+        Scanner sc = new Scanner(System.in);
+        // check pallindrom
+        int N = sc.nextInt();
+        int original = N;
+        int reversed = 0;
 
-        if (X <= 1) {
-            prime = 0; 
-        } else {
-            for (int i = 2; i * i <= X; i++) {
-                if (X % i == 0) {
-                    prime = 0;
-                    break;
-                }
-            }
+        while (N > 0) {
+            int digit = N % 10;
+            reversed = reversed * 10 + digit;
+            N = N / 10;
         }
-
-        if (prime==1) {
+        System.out.println(reversed);
+        if (original == reversed) {
             System.out.println("YES");
         } else {
             System.out.println("NO");
         }
 
         sc.close();
-    
 
+        // Given a number X. Determine if the number is prime or not.
 
-     // Given a number N. Print the factorial of number N.InputFirst line contains a number T (1 ≤ T ≤ 15) number of test cases.Next T lines will contain a number N (0 ≤ N ≤ 20).
-    //  int T = sc.nextInt();
-    //  for(int i=0;i<T;i++){
-    //     int N = sc.nextInt();
-    //     long fact = 1;
-    //     for(int j = 1;j<=N;j++){
-    //         fact =
-    //            fact*j;
-    //     }
-    //     System.out.println(fact);
-        
-     //}
+        // int X = sc.nextInt();
+        // int prime = 1;
 
-     // Given a number N. Print the maltiplication table of the number from 1 to 12.
-    //   int N = sc.nextInt();
-    //   for(int i=1;i<=12;i++){
-    //     System.out.println(N+" "+"*"+" "+i+" "+"="+" "+N*i);
-        
-    //   }
+        // if (X <= 1) {
+        // prime = 0;
+        // } else {
+        // for (int i = 2; i * i <= X; i++) {
+        // if (X % i == 0) {
+        // prime = 0;
+        // break;
+        // }
+        // }
+        // }
 
-     // Given a number N, and N numbers, find maximum number in these N numbers.
+        // if (prime==1) {
+        // System.out.println("YES");
+        // } else {
+        // System.out.println("NO");
+        // }
 
-    //  int N = sc.nextInt();
-    //  int[] arr = new int[N];
-    //  for(int i=0;i<N;i++){
-    //     arr[i] = sc.nextInt();
-        
-    //  }
-    //  int max = arr[0];
-    //  for(int i=0;i<N;i++){
-    //     if(arr[i]>max){
-    //         max = arr[i];
-            
-    //     }
-    //  }
-    //  System.out.println(max);
-     
-     // Given multiple lines each line contains a number X which is a password. Print "Wrong" if the password is incorrect otherwise, print "Correct" and terminate the program.
-     //Note: The "Correct" password is the number 1999
-     
-    //  while(sc.hasNextInt()){
-    //    int X = sc.nextInt();
-    //     if(X==1999){
-    //         System.out.println("Correct");
-    //         break;
-    //     }
-    //     else{
-    //         System.out.println("Wrong");
-    //     } 
-        
-    // }
+        sc.close();
 
+        // Given a number N. Print the factorial of number N.InputFirst line contains a
+        // number T (1 ≤ T ≤ 15) number of test cases.Next T lines will contain a number
+        // N (0 ≤ N ≤ 20).
+        // int T = sc.nextInt();
+        // for(int i=0;i<T;i++){
+        // int N = sc.nextInt();
+        // long fact = 1;
+        // for(int j = 1;j<=N;j++){
+        // fact =
+        // fact*j;
+        // }
+        // System.out.println(fact);
 
-     // Given N numbers. Count how many of these values are even, odd, positive and negative.
-    //  int N = sc.nextInt();
-    //  int i = 1;
-    //  int even = 0;
-    //  int odd = 0;
-    //  int positive = 0;
-    //  int negative = 0;
-    //  while(i<=N){
-    //     int num = sc
-    //        .nextInt();
-    //        if(num%2==0){
-    //          even++;
-             
-    //        }
-    //        if(num%2!=0){
-    //           odd++;
-              
-    //        }
-    //        if(num>0){
-    //          positive++;
-             
-    //        }
-    //        if(num<0){
-    //         negative++;
-            
-    //        }
-    //        i++;
-           
-    //  }
-    //  System.out.println("Even:"+" "+even);
-    //  System.out.println("Odd:"+" "+odd);
-    //  System.out.println("Positive:"+" "+positive);
-    //  System.out.println("Negative:"+" "+negative);
+        // }
 
-     //print all even number from 1 to N and print -1 if there is no even number.
-    //  int N = sc.nextInt();
-    //  int i = 2;
-    //  while(i<=N){
-    //     System.out.println(i);
-    //     i = i+2;
-        
-    //  }
-    
-    //  if(i==2){
-    //     System.out.println(-1);
-        
-    //  }
+        // Given a number N. Print the maltiplication table of the number from 1 to 12.
+        // int N = sc.nextInt();
+        // for(int i=1;i<=12;i++){
+        // System.out.println(N+" "+"*"+" "+i+" "+"="+" "+N*i);
 
+        // }
 
-    //  int N = sc.nextInt();
-    //  int i = 1;
-    //  while(i<=N){
-    //     System.out.println(i);
-    //     i++;
-        
-    //  }
-     
-    
+        // Given a number N, and N numbers, find maximum number in these N numbers.
 
+        // int N = sc.nextInt();
+        // int[] arr = new int[N];
+        // for(int i=0;i<N;i++){
+        // arr[i] = sc.nextInt();
 
-}
+        // }
+        // int max = arr[0];
+        // for(int i=0;i<N;i++){
+        // if(arr[i]>max){
+        // max = arr[i];
+
+        // }
+        // }
+        // System.out.println(max);
+
+        // Given multiple lines each line contains a number X which is a password. Print
+        // "Wrong" if the password is incorrect otherwise, print "Correct" and terminate
+        // the program.
+        // Note: The "Correct" password is the number 1999
+
+        // while(sc.hasNextInt()){
+        // int X = sc.nextInt();
+        // if(X==1999){
+        // System.out.println("Correct");
+        // break;
+        // }
+        // else{
+        // System.out.println("Wrong");
+        // }
+
+        // }
+
+        // Given N numbers. Count how many of these values are even, odd, positive and
+        // negative.
+        // int N = sc.nextInt();
+        // int i = 1;
+        // int even = 0;
+        // int odd = 0;
+        // int positive = 0;
+        // int negative = 0;
+        // while(i<=N){
+        // int num = sc
+        // .nextInt();
+        // if(num%2==0){
+        // even++;
+
+        // }
+        // if(num%2!=0){
+        // odd++;
+
+        // }
+        // if(num>0){
+        // positive++;
+
+        // }
+        // if(num<0){
+        // negative++;
+
+        // }
+        // i++;
+
+        // }
+        // System.out.println("Even:"+" "+even);
+        // System.out.println("Odd:"+" "+odd);
+        // System.out.println("Positive:"+" "+positive);
+        // System.out.println("Negative:"+" "+negative);
+
+        // print all even number from 1 to N and print -1 if there is no even number.
+        // int N = sc.nextInt();
+        // int i = 2;
+        // while(i<=N){
+        // System.out.println(i);
+        // i = i+2;
+
+        // }
+
+        // if(i==2){
+        // System.out.println(-1);
+
+        // }
+
+        // int N = sc.nextInt();
+        // int i = 1;
+        // while(i<=N){
+        // System.out.println(i);
+        // i++;
+
+        // }
+
+    }
 }
