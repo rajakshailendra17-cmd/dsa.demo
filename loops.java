765ff3fe95967a19f4bde2427a7aa08049e60da0
shailendra rajak <rajakshailendra17@gmail.com>
2026-09-17 00:52:35 +0530

    check password

--- loops.java
+++ loops.java
@@ -3,42 +3,56 @@ import java.util.Scanner;
 public class loops {
     public static void main(String[] args) {
      Scanner sc = new Scanner(System.in);
-     // Given N numbers. Count how many of these values are even, odd, positive and negative.
-     int N = sc.nextInt();
-     int i = 1;
-     int even = 0;
-     int odd = 0;
-     int positive = 0;
-     int negative = 0;
-     while(i<=N){
-        int num = sc
-           .nextInt();
-           if(num%2==0){
-             even++;
-             
-           }
-           if(num%2!=0){
-              odd++;
-              
-           }
-           if(num>0){
-             positive++;
-             
-           }
-           if(num<0){
-            negative++;
-            
-           }
-           i++;
-           
-     }
-     System.out.println("Even:"+" "+even);
-     System.out.println("Odd:"+" "+odd);
-     System.out.println("Positive:"+" "+positive);
-     System.out.println("Negative:"+" "+negative);
+     // Given multiple lines each line contains a number X which is a password. Print "Wrong" if the password is incorrect otherwise, print "Correct" and terminate the program.
+     //Note: The "Correct" password is the number 1999
+     
+     while(sc.hasNextInt()){
+       int X = sc.nextInt();
+        if(X==1999){
+            System.out.println("Correct");
+            break;
+        }
+        else{
+            System.out.println("Wrong");
+        } 
+        
+    }
 
      sc.close();
 
+     // Given N numbers. Count how many of these values are even, odd, positive and negative.
+    //  int N = sc.nextInt();
+    //  int i = 1;
+    //  int even = 0;
+    //  int odd = 0;
+    //  int positive = 0;
+    //  int negative = 0;
+    //  while(i<=N){
+    //     int num = sc
+    //        .nextInt();
+    //        if(num%2==0){
+    //          even++;
+             
+    //        }
+    //        if(num%2!=0){
+    //           odd++;
+              
+    //        }
+    //        if(num>0){
+    //          positive++;
+             
+    //        }
+    //        if(num<0){
+    //         negative++;
+            
+    //        }
+    //        i++;
+           
+    //  }
+    //  System.out.println("Even:"+" "+even);
+    //  System.out.println("Odd:"+" "+odd);
+    //  System.out.println("Positive:"+" "+positive);
+    //  System.out.println("Negative:"+" "+negative);
 
      //print all even number from 1 to N and print -1 if there is no even number.
     //  int N = sc.nextInt();
