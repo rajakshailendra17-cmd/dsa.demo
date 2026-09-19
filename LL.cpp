00b8fbde21f72f61231d92ead71773e7fc51d1da
shailendra rajak <rajakshailendra17@gmail.com>
2026-09-19 14:17:48 +0530

    insert,search

--- LL.cpp
+++ LL.cpp
@@ -22,7 +22,7 @@ public:
     {
         head = tail = NULL;
     }
-    void push_front(int val)
+    void push_front(int val) // O(1)
     {
         Node *new_node = new Node(val);
         if (head == NULL)
@@ -36,7 +36,7 @@ public:
             head = new_node;
         }
     }
-    void push_back(int val)
+    void push_back(int val) // O(1)
     {
         Node *new_node = new Node(val);
         if (head == NULL)
@@ -49,7 +49,7 @@ public:
             tail = new_node;
         }
     }
-    void pop_front()
+    void pop_front() // O(1)
     {
         if (head == NULL)
         {
@@ -68,7 +68,8 @@ public:
         }
     }
 
-    void pop_back()
+    void pop_back() // O(n)
+
     {
         if (head == NULL)
         {
@@ -90,8 +91,37 @@ public:
         tail = temp;       // update tail
         tail->next = NULL; // mark end of list
     }
+    void insert(int val, int pos) // O(n)
+    {
+        if (pos < 0)
+        {
+            cout << "invalid position" << endl;
+            return;
+        }
+        else if (pos == 0)
+        {
+            push_front(val);
+            return;
+        }
+        else
+        {
+            Node *temp = head;
+            for (int i = 0; i < pos - 1; i++)
+            {
+                if (temp == NULL)
+                {
+                    cout << "invalid position" << endl;
+                    return;
+                }
+                temp = temp->next;
+            }
+            Node *newNode = new Node(val);
+            newNode->next = temp->next;
+            temp->next = newNode;
+        }
+    }
 
-    void printll()
+    void printll() // O(n)
     {
         Node *temp = head;
         while (temp != NULL)
@@ -100,6 +130,22 @@ public:
             temp = temp->next;
         }
         cout << "NULL" << endl;
+    }
+    int search(int key) // O(n)
+    {
+        Node *temp = head;
+        int pos = 0;
+        while (temp != NULL)
+        {
+            if (temp->data == key)
+            {
+                return pos;
+                break;
+            }
+            pos++;
+            temp = temp->next;
+        }
+        return -1;
     }
 };
 int main()
@@ -110,11 +156,24 @@ int main()
     l.push_front(3);
     l.push_front(4);
     l.push_back(5);
-    l.pop_front();
 
     l.printll();
-    l.pop_back();
-    l.printll();
+    // l.insert(6, 1);
+    // l.printll();
+    cout << "enter a number to search"
+         << endl;
+    int key;
+    cin >> key;
+    int pos = l.search(key);
+    if (pos == -1)
+    {
+        cout << "not found" << endl;
+    }
+    else
+    {
+
+        cout << "found at position " << pos << endl;
+    }
 
     return 0;
 }
