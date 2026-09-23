0077d9ec7c306a643b309b036546cfd4c869a888
shailendra rajak <rajakshailendra17@gmail.com>
2026-09-23 22:06:59 +0530

    searching

--- array.java
+++ array.java
@@ -4,17 +4,41 @@ import java.util.Arrays;
 public class array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // Given a number N and an array A of N numbers. Determine if the number X
+        // exists in array A or not and print its position (0-index).
+
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+        int X = sc.nextInt();
+        int i;
+        int count = 0;
+        for (i = 0; i < N; i++) {
+            if (A[i] == X) {
+                System.out.println(i);
+                count = 1;
+                break;
+            }
+
+        }
+        if (count == 0) {
+            System.out.println(-1);
+
+        }
+
         // Given a number N and an array A of N numbers. Print the absolute summation of
         // these numbers.
 
-        int N = sc.nextInt();
-        long sum = 0;
+        // int N = sc.nextInt();
+        // long sum = 0;
 
-        for (int i = 0; i < N; i++) {
-            sum += sc.nextInt();
-        }
+        // for (int i = 0; i < N; i++) {
+        // sum += sc.nextInt();
+        // }
 
-        System.out.println(Math.abs(sum));
+        // System.out.println(Math.abs(sum));
         sc.close();
     }
 }
--- dsa.py
+++ dsa.py
@@ -1,15 +1,34 @@
-def twoSum(nums, target):
-    seen = {} 
+# def twoSum(nums, target):
+#     seen = {} 
     
-    for i, num in enumerate(nums):
-        complement = target - num
-        if complement in seen:
-            return [seen[complement], i]
-        seen[num] = i
+#     for i, num in enumerate(nums):
+#         complement = target - num
+#         if complement in seen:
+#             return [seen[complement], i]
+#         seen[num] = i
 
 
-nums = [2, 7, 11, 15]
-target = 9
-print(twoSum(nums, target)) 
+# nums = [2, 7, 11, 15]
+# target = 9
+# print(twoSum(nums, target)) 
 
-    
+import colorsys
+import turtle
+screen = turtle.Screen()
+screen.bgcolor("black")
+t = turtle.Turtle()
+t.speed(0)
+t.width(1)
+n, h=36,0
+for i in range(200):
+    c=colorsys.hsv_to_rgb(h, 1, 1)
+    t.color(c)
+    h+=1/n
+    t.forward(i)
+    t.left(135)
+    t.forward(i)
+    t.left(30)
+    t.circle(i, 45)
+turtle.done
+
+
