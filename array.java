078e85249298783fd76b831ccaa783653c9089a7
shailendra rajak <rajakshailendra17@gmail.com>
2026-09-24 14:53:38 +0530

    replacement

--- array.java
+++ array.java
@@ -4,29 +4,48 @@ import java.util.Arrays;
 public class array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // Given a number N and an array A of N numbers. Determine if the number X
-        // exists in array A or not and print its position (0-index).
-
         int N = sc.nextInt();
         int[] A = new int[N];
         for (int i = 0; i < N; i++) {
             A[i] = sc.nextInt();
         }
-        int X = sc.nextInt();
-        int i;
-        int count = 0;
-        for (i = 0; i < N; i++) {
-            if (A[i] == X) {
-                System.out.println(i);
-                count = 1;
-                break;
+        for (int i = 0; i < N; i++) {
+            if (A[i] > 0) {
+                A[i] = 1;
+            } else if (A[i] < 0) {
+                A[i] = 2;
+            } else {
+                A[i] = A[i];
             }
-
         }
-        if (count == 0) {
-            System.out.println(-1);
 
+        for (int i = 0; i < N; i++) {
+            System.out.print(A[i] + " ");
         }
+
+        // Given a number N and an array A of N numbers. Determine if the number X
+        // exists in array A or not and print its position (0-index).
+
+        // int N = sc.nextInt();
+        // int[] A = new int[N];
+        // for (int i = 0; i < N; i++) {
+        // A[i] = sc.nextInt();
+        // }
+        // int X = sc.nextInt();
+        // int i;
+        // int count = 0;
+        // for (i = 0; i < N; i++) {
+        // if (A[i] == X) {
+        // System.out.println(i);
+        // count = 1;
+        // break;
+        // }
+
+        // }
+        // if (count == 0) {
+        // System.out.println(-1);
+
+        // }
 
         // Given a number N and an array A of N numbers. Print the absolute summation of
         // these numbers.
--- dsa.py
+++ dsa.py
@@ -12,23 +12,27 @@
 # target = 9
 # print(twoSum(nums, target)) 
 
-import colorsys
-import turtle
-screen = turtle.Screen()
-screen.bgcolor("black")
-t = turtle.Turtle()
-t.speed(0)
-t.width(1)
-n, h=36,0
-for i in range(200):
-    c=colorsys.hsv_to_rgb(h, 1, 1)
-    t.color(c)
-    h+=1/n
-    t.forward(i)
-    t.left(135)
-    t.forward(i)
-    t.left(30)
-    t.circle(i, 45)
-turtle.done
+# import colorsys
+# import turtle
+# screen = turtle.Screen()
+# screen.bgcolor("black")
+# t = turtle.Turtle()
+# t.speed(0)
+# t.width(1)
+# n, h=36,0
+# for i in range(200):
+#     c=colorsys.hsv_to_rgb(h, 1, 1)
+#     t.color(c)
+#     h+=1/n
+#     t.forward(i)
+#     t.left(135)
+#     t.forward(i)
+#     t.left(30)
+#     t.circle(i, 45)
+# turtle.done
+# // print 1 to N numbers in a single line using a single loop
+# N = int(input("enter any number: "));
 
+# for i in range(1, 10):
+#     print(i)
 
